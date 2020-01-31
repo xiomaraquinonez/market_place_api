fa772b28c9819c9dc32b0386feb64c1623752c3e
Xiomara Quinonez <xiomaraxquinonez@gmail.com>
2020-01-31 11:19:07 -0800

    chapter 2 updates

--- lib/spec/api_constraints_spec.rb
+++ lib/spec/api_constraints_spec.rb
@@ -17,3 +17,14 @@ describe ApiConstraints do
     end
   end
 end
+
+class ApiConstraints
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || req.headers['Accept'].include?("application/vnd.marketplace.v#{@version}")
+  end
+end
